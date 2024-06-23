c4e60dfa208bbdfdfc5a3f85feba0819b2b29aa4
dylan <dylan.slyter@yahoo.com>
2024-06-23 19:18:36 -0400

    changing folders around

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Text Editor Starter Code
+This is a text editor that stores your data and will stilll be there when refreshed.
+The build works perfectly on local host and through the terminal but for some reason it just wont work in render, and im not sure why it wont work.
--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,7 +1,7 @@
 const path = require('path');
-const filepath = "/Users/dylanslyter/Desktop/19-DS-PWA's/client/dist/index.html";
+let filepath = process.env.CLIENT_PATH || "/Users/dylanslyter/Desktop/19-DS-PWA's/client/dist";
+filepath += "/index.html";
 module.exports = (app) =>
   app.get('/', (req, res) =>
-    //res.sendFile(path.join(__dirname, '../client/dist/index.html'))
     res.sendFile(filepath)
   );
