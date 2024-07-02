3c4bd7895d4c10d195d3ded60f7a6eb7e9dc1c21
dylan <dylan.slyter@yahoo.com>
2024-07-01 22:24:00 -0400

    fixing issues

--- server/server.js
+++ server/server.js
@@ -2,11 +2,10 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const filepath = process.env.CLIENT_PATH || "/Users/dylanslyter/Desktop/19-DS-PWA's/client/dist";
-app.use(express.static(filepath));
+app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./server/routes/htmlRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
